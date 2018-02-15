0475639eecd67fd6de8d8debaa0136ffb4219fe3
Akshay Kadam <akshaykadam0904@gmail.com>
2018-02-15 17:37:37 +0530

    Lodash with TreeShaking 💩

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import { sum } from "./math";
 
+import map from "lodash/map";
+
 console.log("💩");
 
 console.log(sum(2, 3));
+
+map([1, 2, 3], x => {
+  console.log(x);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
-
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const config = {
   entry: "./app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js"
-  }
+  },
+  plugins: [new UglifyJSPlugin()]
 };
 
 module.exports = config;
